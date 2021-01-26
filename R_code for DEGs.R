853e558c58a35afa1bae42b97b8e4219d445f085
Eskezeia Y. Dessie <67934822+estu2003@users.noreply.github.com>
2021-01-26 17:59:42 +0800

    Update R_code for DEGs.R

--- R_code for DEGs.R
+++ R_code for DEGs.R
@@ -4,20 +4,20 @@
 
 library(limma)
 library(edgeR)
-setwd("C:/Users/Reon-Bruk/Desktop/geneT2/kidney2")
-dat1<-read.csv("mRNA_KIRC.csv",row.names=1)
+setwd("C:/Users/Reon-Bruk/Desktop/geneT2/HCC")
+dat1<-read.csv("mRNA_HCC.csv",row.names=1)
 dim(dat1)
 dat2<-log2(dat1 + 1)
 
 dat3<-as.matrix(dat2)
 dim(dat3)
-design.mat<-read.csv("design_KIRC_gene.csv")
+design.mat<-read.csv("design_HCC_gene.csv")
 design.mat<-design.mat[,-1]
 design.mat
 dim(design.mat)
 contrast.mat<-matrix(c(1,-1), ncol = 1)
 dimnames(contrast.mat)<-list(c('T', 'N'), "Diff")
-sample<-factor(rep(c("T", "N"), c(534,72)))
+sample<-factor(rep(c("T", "N"), c(371,50)))
 design.mat<-model.matrix(~0+sample)
 colnames(design.mat)<-levels(sample)
 design.mat
@@ -34,7 +34,6 @@ dat1<-read.csv("HCC_FiveGene_TPM_survival2.csv")
 write.csv(dat1, "dat.csv")
 dat2<-read.csv("dat.csv")
 head(dat2)
-
 model<- coxph(Surv(os, event) ~ dat2$LCATx, data =dat2)
 summary(model)
 model<- coxph(Surv(y.time,y.status) ~ RSx, data =dat1)
